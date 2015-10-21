500db7d0d8a599a2fb2a6db7f7aad7335dcdd8a1
Jonathan D. Lace <jdlace@icloud.com>
2015-10-21 19:00:07 -0400

    Initial Commit

---
+++ 2.1 Functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2.1 Functions
+//
+//  Created by Jonathan Lace on 10/21/15.
+//  Copyright © 2015 shp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
